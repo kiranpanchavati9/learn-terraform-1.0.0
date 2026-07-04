5836f3771d47d2de97c83ff8323bbea3404a652e
kiranpanchavati9 <kiranpanchavati9@gmail.com>
2026-07-04 17:37:51 +0400

    example updated

--- datasources/main.tf
+++ datasources/main.tf
@@ -1,13 +1,14 @@
 data "aws_ami" "example" {
-  owners           = ["973714476888"]
   filter {
     name   = "name"
     values = ["RHEL-9-DevOps-Practice"]
   }
+
+  owners           = ["973714476888"] # This is the AWS account ID for the RHEL 9 DevOps Practice AMI   
 }
 
 output "ami" {
-  value = data.aws_ami.example.id # This is the AMI ID for the RHEL 9 DevOps Practice AMI
+  value = data.aws_ami.example # This is the AMI ID for the RHEL 9 DevOps Practice AMI
 }
 
 provider "aws" {
