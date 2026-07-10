79a6915a1eaea7729d648212f87c7afd5a312446
kiranpanchavati9 <kiranpanchavati9@gmail.com>
2026-07-10 21:24:13 +0400

    state file updated

---
+++ state/maint.tf
@@ -0,0 +1,11 @@
+terraform {
+    backend "s3" {
+        bucket = "terraform-state-bucket-030794"
+        key = "terraform.tfstate"
+        region = "us-east-1"
+    }
+}
+
+output "bucket_name" {
+    value = aws_s3_bucket.terraform_state.bucket
+}
