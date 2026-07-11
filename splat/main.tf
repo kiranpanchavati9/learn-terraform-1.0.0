584dfc0243422aeaffceceffa251a10500603c52
kiranpanchavati9 <kiranpanchavati9@gmail.com>
2026-07-11 10:50:53 +0400

    mmap

--- splat/main.tf
+++ splat/main.tf
@@ -10,6 +10,21 @@ variable "ports" {
 }
 
 
+variable "dummy_ports" {
+    default = {
+        port1 = 9090
+        port2 = 9091
+        port3 = 9092
+        port4 = 9093
+        port5 = 9094
+        port6 = 9095
+    }
+}
+
 output "list" {
     value = [ for x,y in var.ports : y ]
+}
+
+output "mmap" {
+    value = { for x,y in var.dummy_ports : "${x}" => y }
 }
